0e31dbbe09abe8c27db3db88eca02871b63dfda3
MFaisal125 <mfaisal0125470@gmail.com>
2024-11-04 13:26:39 +0500

    Hotel Booking

--- components/shared/loading/Screen.js
+++ components/shared/loading/Screen.js
@@ -3,9 +3,9 @@ import LoadImage from "../image/LoadImage";
 
 const Screen = () => {
   return (
-    <section className="flex justify-center items-center h-screen w-screen bg-secondary/10">
+    <section className="flex justify-center items-center h-screen w-screen bg-secondary/10 bg-green-200">
       <LoadImage
-        src="/loading.png"
+        src="/loading.gif"
         alt="loading"
         height={300}
         width={300}
